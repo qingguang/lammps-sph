4159533ea2c3656f01da706f43823d544a8103b2
Litvinov Sergey <slitvinov@gmail.com>
2011-11-10 12:37:50 +0100

    Add a term in the dissipative force (see eq (30) in Hu2006, doi:10.1016/j.jcp.2005.09.001)

--- src/USER-SPH/pair_sdpd.cpp
+++ src/USER-SPH/pair_sdpd.cpp
@@ -213,13 +213,14 @@ void PairSDPD::compute(int eflag, int vflag) {
           }
         }
 
+        const double VdotE = eij[0]*velx + eij[1]*vely + eij[2]*velz;
         fpair = -imass * jmass * (fi + fj) * wfd;
         deltaE = -0.5 *(fpair * delVdotDelR + fvisc * (velx*velx + vely*vely + velz*velz));
         //modify force pair
-        f[i][0] += delx * fpair + velx * fvisc+_dUi[0];
-        f[i][1] += dely * fpair + vely * fvisc+_dUi[1];
+        f[i][0] += delx*fpair + velx*fvisc + eij[0]*VdotE*fvisc + _dUi[0];
+        f[i][1] += dely*fpair + vely*fvisc + eij[1]*VdotE*fvisc + _dUi[1];
 	if (domain->dimension ==3 ) {
-	  f[i][2] += delz * fpair + velz * fvisc +_dUi[2];
+	  f[i][2] += delz*fpair + velz*fvisc + eij[2]*VdotE*fvisc + _dUi[2];
 	}
 
         // and change in density
@@ -229,10 +230,10 @@ void PairSDPD::compute(int eflag, int vflag) {
         de[i] += deltaE;
 
         if (newton_pair || j < nlocal) {
-          f[j][0] -= delx * fpair + velx * fvisc+_dUi[0];
-          f[j][1] -= dely * fpair + vely * fvisc+_dUi[1];
+          f[j][0] -= delx*fpair + velx*fvisc + eij[0]*VdotE*fvisc + _dUi[0];
+          f[j][1] -= dely*fpair + vely*fvisc + eij[1]*VdotE*fvisc + _dUi[1];
 	  if (domain->dimension ==3 ) {
-	    f[j][2] -= delz * fpair + velz * fvisc+_dUi[2];
+	    f[j][2] -= delz*fpair + velz*fvisc + eij[2]*VdotE*fvisc + _dUi[2];
 	  }
           de[j] += deltaE;
           drho[j] += imass * delVdotDelR * wfd;
