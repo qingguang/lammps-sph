211537dcb4f83a070da1c9ee7d48bc75e2f28313
Xie Qingguang <qingguang@nana.aer.mw.tum.de>
2011-12-08 16:06:57 +0100

    add special bond

--- examples/USER/sph/sdpd-polymer/run.sh
+++ examples/USER/sph/sdpd-polymer/run.sh
@@ -7,10 +7,10 @@
 ../../../../tools/restart2data poly.restart poly.txt
 
 
- awk -v cutoff=3.0 -v Nbeads=3 -v Nsolvent=1 -v Npoly=full \
+ awk -v cutoff=3.0 -v Nbeads=9 -v Nsolvent=18 -v Npoly=full \
      -f addpolymer.awk poly.txt > poly2.txt
  nbound=$(tail -n 1 poly2.txt | awk '{print $1}')
  sed "s/_NUMBER_OF_BOUNDS_/$nbound/1" poly2.txt > poly.txt
 
-mpirun  -np 2 ../../../../src/lmp_linux -in sdpd-polymer-run.lmp
+time /scratch/qingguang/prefix-nana/bin/mpirun -np 4  ../../../../src/lmp_linux -in sdpd-polymer-run.lmp
 #../../../../src/lmp_linux -in sdpd-polymer-run.lmp
