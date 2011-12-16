9206ee5d3ebb96c1d76d43235d03dbf0cfff5330
Xie Qingguang <qingguang@nana.aer.mw.tum.de>
2011-12-16 15:21:50 +0100

    Create a template for wall bounded polymer solution
    Former-commit-id: 6fc819f99a702f3ae16c3f2d6cf73d483df27595

---
+++ examples/USER/sph/sdpd-polymer-wall/run3d.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+
+
+
+../../../../src/lmp_linux -in sdpd-polymer3D-inti.lmp
+../../../../tools/restart2data poly3d.restart poly3d.txt
+
+
+ awk -v cutoff=3.0 -v Nbeads=3 -v Nsolvent=1 -v Npoly=full \
+     -f addpolymer.awk poly3d.txt > poly3.txt
+ nbound=$(tail -n 1 poly3.txt | awk '{print $1}')
+ sed "s/_NUMBER_OF_BOUNDS_/$nbound/1" poly3.txt > poly3d.txt
+
+mpirun  -np 2 ../../../../src/lmp_linux -in sdpd-polymer3D-run.lmp
+#../../../../src/lmp_linux -in sdpd-polymer-run.lmp
