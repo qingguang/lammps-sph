02c64742d5a78548bb23b8c73b11199e6f309290
Xie Qingguang <qingguang@nana.aer.mw.tum.de>
2012-02-24 11:36:24 +0100

    creat sdpd-polymer wall and fedenov

---
+++ examples/USER/sph/sdpd-3d-polymer-fedosov/run3d.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+
+
+rm -rf dum* im* poly* log.lammps
+../../../../src/lmp_linux -in sdpd-polymer3D-inti.lmp
+../../../../tools/restart2data poly3d.restart poly3d.txt
+
+
+ awk -v cutoff=3.0 -v Nbeads=2 -v Nsolvent=0 -v Npoly=full \
+     -f addpolymer.awk poly3d.txt > poly3.txt
+ nbound=$(tail -n 1 poly3.txt | awk '{print $1}')
+ sed "s/_NUMBER_OF_BOUNDS_/$nbound/1" poly3.txt > poly3d.txt
+
+time /scratch/qingguang/prefix-nana/bin/mpirun -np 6  ../../../../src/lmp_linux -in sdpd-polymer3D-run.lmp
+#mpirun  -np 2 ../../../../src/lmp_linux -in sdpd-polymer3D-run.lmp
+#../../../../src/lmp_linux -in sdpd-polymer-run.lmp
