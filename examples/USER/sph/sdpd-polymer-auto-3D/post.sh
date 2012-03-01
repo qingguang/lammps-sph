f1cbde2578bf668c17e89a04d43e736336e1cf08
Xie Qingguang <qingguang@nana.aer.mw.tum.de>
2012-03-01 16:50:55 +0100

    configure file for wall in sdpd

--- examples/USER/sph/sdpd-polymer-auto-3D/post.sh
+++ examples/USER/sph/sdpd-polymer-auto-3D/post.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 rm -rf punto.dat
 rm -rf pdata
-awk 'fl{print $3, $4, $5, $6} /ITEM: ATOMS/{fl=1}' *.dat > punto.dat
+awk 'fl{print $3, $4, $5, $6,$7,$8} /ITEM: ATOMS/{fl=1}' *.dat > punto.dat
 awk -f extpolymer.awk dump*.dat
 # write auto-correlation to corr.dat
 octave -q --eval relaxtime
