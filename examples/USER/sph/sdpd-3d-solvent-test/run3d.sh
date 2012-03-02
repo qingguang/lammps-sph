a094a643dca1926d328a5bbc4a0778144b277add
Xie Qingguang <qingguang@nana.aer.mw.tum.de>
2012-03-02 15:51:22 +0100

    Befor testing deform

--- examples/USER/sph/sdpd-3d-solvent-test/run3d.sh
+++ examples/USER/sph/sdpd-3d-solvent-test/run3d.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 
-time /scratch/qingguang/prefix-nana/bin/mpirun -np 6  ../../../../src/lmp_linux -in sdpd_test_3d.lmp
+time /scratch/qingguang/prefix-nana/bin/mpirun -np 1  ../../../../src/lmp_linux -in sdpd_test_3d.lmp
 #../../../../src/lmp_linux -in sdpd_test_3d.lmp
