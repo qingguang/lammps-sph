b18b966aff1dcab508f4ca453ede37ec65111f65
xieqg <xieqg@ubuntu.(none)>
2012-05-11 09:11:06 +0200

    commit run36.sh

--- examples/USER/sph/sdpd-3d-polymer-fedosov/post.sh
+++ examples/USER/sph/sdpd-3d-polymer-fedosov/post.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 rm -rf punto.dat
 rm -rf pdata
-awk 'fl{print $3, $4, $5, $6,$7,$8} /ITEM: ATOMS/{fl=1}' dumpreal*.dat > punto.dat
-awk -f extpolymer.awk dumpreal*.dat
-#octave -q --eval "ext"
-
+#awk 'fl{print $3, $4, $5, $6,$7,$8} /ITEM: ATOMS/{fl=1}' *.dat > punto.dat
+awk -f extpolymer.awk dump0*.dat
+# write auto-correlation to corr.dat
+#octave -q --eval ext
+#gnuplot fitcorr.gp
