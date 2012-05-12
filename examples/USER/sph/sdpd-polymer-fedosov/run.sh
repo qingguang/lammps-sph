a49e0b16e9406e2c8fb0193ba02d5417b4454196
Litvinov Sergey <slitvinov@gmail.com>
2012-05-12 13:44:25 +0200

    Make scripts for 2D and 3D

--- examples/USER/sph/sdpd-polymer-fedosov/run.sh
+++ examples/USER/sph/sdpd-polymer-fedosov/run.sh
@@ -10,14 +10,14 @@ else
     exit -1
 fi
 
+ndim=3d
 rm -rf dum* im* poly* log.lammps
-${lmp} -in sdpd-polymer2D-inti.lmp
+${lmp} -var ndim ${ndim} -in sdpd-polymer-init.lmp
 ${restart2data} poly3d.restart poly3d.txt
-
 
  awk -v cutoff=3.0 -v Nbeads=0 -v Nsolvent=1 -v Npoly=full \
      -f addpolymer.awk poly3d.txt > poly3.txt
  nbound=$(tail -n 1 poly3.txt | awk '{print $1}')
  sed "s/_NUMBER_OF_BOUNDS_/$nbound/1" poly3.txt > poly3d.txt
 
-${mpirun}  -np 2 ${lmp} -in sdpd-polymer2D-run.lmp
+${mpirun}  -np 2 ${lmp} -var ndim ${ndim} -in sdpd-polymer-run.lmp
