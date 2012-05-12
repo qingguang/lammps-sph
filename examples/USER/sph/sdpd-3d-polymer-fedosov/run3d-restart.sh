aa9a61b0b81f006e9c13af0e41dd41f57a9b85a3
Litvinov Sergey <slitvinov@gmail.com>
2012-05-12 13:30:28 +0200

    Update of 3D polymer configuration

---
+++ examples/USER/sph/sdpd-3d-polymer-fedosov/run3d-restart.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -e
+set -u
+configfile=$HOME/lammps-sph.sh
+if [ -f "${configfile}" ]; then
+    source "${configfile}"
+else
+    printf "cannot find config file: %s\n" ${configfile} > "/dev/stderr"
+    exit -1
+fi
+
+${mpirun}  -np 8 ${lmp} -in sdpd-polymer3D-restart.lmp
