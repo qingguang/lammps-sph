1d08d4c6fe76a1e4fe10e040aa8438f2bcdd8c39
Xie Qingguang <qingguang@nana.aer.mw.tum.de>
2012-07-27 14:56:02 +0200

    Add post-processing

--- examples/USER/sph/script/pje.sh
+++ examples/USER/sph/script/pje.sh
@@ -8,7 +8,7 @@ for filename in dump*.dat; do echo $filename;
 #awk 'fl{print $3, $4, $5, $6,$7,$8} /ITEM: ATOMS/{fl=1}' $filename > ${filename/.dat/.del};
 #Format: zs_sph_projection num_dim num_particles L Overlap input_filename file_type kernel_type projection_order
 awk 'fl{print $3, $4, $6, $7} /ITEM: ATOMS/{fl=1}' $filename > ${filename/.dat/.del};
-./zs_mls_projection 2 96 0.08 1.9 ${filename/.dat/.del} 2 2 2 ;
+./zs_mls_projection 2 96 0.08 2.0 ${filename/.dat/.del} 2 2 2 ;
 done
 #awk 'fl{print $3, $4, $5, $6,$7,$8} /ITEM: ATOMS/{fl=1}' dump0*.dat > projection*.dat
 #./zs_mls_projection 3 20 1 1.8 dump*.del 2 2 2
