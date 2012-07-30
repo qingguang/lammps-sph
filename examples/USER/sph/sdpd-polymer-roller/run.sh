79421b1e489311256c9ca40aa37f8a09837af545
Litvinov Sergey <slitvinov@gmail.com>
2012-07-30 19:35:11 +0200

    Add lmp files for harmonic spring

--- examples/USER/sph/sdpd-polymer-roller/run.sh
+++ examples/USER/sph/sdpd-polymer-roller/run.sh
@@ -10,7 +10,7 @@ else
     exit -1
 fi
 
-nproc=6
+nproc=4
 ndim=2d
 
 cp ${ndim}-vars.lmp ${ndim}-model.lmp
@@ -21,9 +21,15 @@ ${restart2data} poly3d.restart poly3d.txt
 
 # -v Nbeads=10 -v Nsolvent=10 -v Npoly=full gives one half of the
 # -domain filled with polymers
- awk -v cutoff=3.0 -v Nbeads=6 -v Nsolvent=6 -v Npoly=full \
+Nbeads=2
+Nsolvent=2
+dname=harmonic-nb${Nbeads}-ns${Nsolvent}-H100
+ awk -v cutoff=3.0 -v Nbeads=${Nbeads} -v Nsolvent=${Nsolvent} -v Npoly=full \
      -f addpolymer.awk poly3d.txt > poly3.txt
  nbound=$(tail -n 1 poly3.txt | awk '{print $1}')
  sed "s/_NUMBER_OF_BOUNDS_/$nbound/1" poly3.txt > poly3d.txt
 
-${mpirun} -np ${nproc} ${lmp} -var ndim ${ndim} -in sdpd-polymer-run.lmp
+# output directory name
+
+mkdir -p ${dname}
+${mpirun} -np ${nproc} ${lmp} -var ndim ${ndim} -var dname ${dname} -in sdpd-polymer-run.lmp
