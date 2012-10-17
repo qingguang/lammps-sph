7660630b0933d2d6ee5059869451fd798d30b0d3
Litvinov Sergey <slitvinov@gmail.com>
2012-10-17 16:59:04 +0200

    Run with 8 processor

--- examples/USER/sph/sph-rising-bubble/run.sh
+++ examples/USER/sph/sph-rising-bubble/run.sh
@@ -2,5 +2,5 @@
 
 dname=data
 mkdir -p ${dname}
-mpirun -np 2  ../../../../src/lmp_linux -in bubble3d.lmp -var dname ${dname}
+mpirun -np 8  ../../../../src/lmp_linux -in bubble3d.lmp -var dname ${dname}
 
