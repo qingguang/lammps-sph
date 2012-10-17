079913cada362dd820158a7ea71493d33a94831a
Litvinov Sergey <slitvinov@gmail.com>
2012-10-17 16:58:54 +0200

    Ouptut sphere in the postprocessing

--- examples/USER/sph/sph-rising-bubble/post3d.sh
+++ examples/USER/sph/sph-rising-bubble/post3d.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 # x, y, fx, fy, colorgradient_peratom
 ../script/lammps2punto.sh data/dump*.*  | awk 'NF{print $3, $4, $5, $9, $10, $11, $2, $NF} !NF' > hm
+../script/lammps2punto.sh data/dump*.*  | awk 'NF&&$2==2{print $3, $4, $5, $9, $10, $11, $2, $NF} !NF' > sp
+../script/lammps2punto.sh data/dump*.*  | awk 'NF&&$2==2{print $3, $5, $4, $9, $11, $10, $2, $NF} !NF' > tp
