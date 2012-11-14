1f4b016544a258992a2b308decce5f0bedf8ff7c
Litvinov Sergey <slitvinov@gmail.com>
2012-11-14 17:55:43 +0100

    A setup to run solvent diffusion test

--- examples/USER/sph/sdpd-polymer-fedosov/run.sh
+++ examples/USER/sph/sdpd-polymer-fedosov/run.sh
@@ -10,7 +10,7 @@ else
     exit -1
 fi
 
-nproc=6
+nproc=8
 ndim=3d
 
 cp ${ndim}-vars.lmp ${ndim}-model.lmp
@@ -19,7 +19,7 @@ rm -rf dum* im* poly* log.lammps *.av
 ${lmp} -var ndim ${ndim} -in sdpd-polymer-init.lmp
 ${restart2data} poly3d.restart poly3d.txt
 
- awk -v cutoff=3.0 -v Nbeads=0 -v Nsolvent=1 -v Npoly=full \
+ awk -v cutoff=3.0 -v Nbeads=25 -v Nsolvent=25 -v Npoly=full \
      -f addpolymer.awk poly3d.txt > poly3.txt
  nbound=$(tail -n 1 poly3.txt | awk '{print $1}')
  sed "s/_NUMBER_OF_BOUNDS_/$nbound/1" poly3.txt > poly3d.txt
