74af3347db5acd68d7516692977121f59a26c01b
Xie Qingguang <qingguang@nana.aer.mw.tum.de>
2012-12-10 14:06:03 +0100

    add stress and run.sh

---
+++ examples/USER/sph/sdpd-polymer-3D-defrom/run.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+set -e
+set -u
+configfile=$HOME/lammps-sph.sh
+if [ -f "${configfile}" ]; then
+    source "${configfile}"
+else
+    printf "cannot find config file: %s\n" ${configfile} > "/dev/stderr"
+    exit -1
+fi
+
+
+nproc=6
+ndim=3d
+Nbeads=0
+Nsolvent=1
+nx=20
+Force=0
+etas=1.5e-2
+rate=4e2
+dname=fene-nb${Nbeads}-ns${Nsolvent}-nx${nx}-H1-R0-f${Force}-rate${rate}
+
+vars="-var nx ${nx} -var ndim ${ndim} -var dname ${dname} -var force ${Force} -var etas ${etas} -var rate ${rate}"
+
+${lmp} ${vars} -in sdpd-polymer-init.lmp
+${restart2data} poly3d.restart poly3d.txt
+
+
+ awk -v cutoff=3.0 -v Nbeads=${Nbeads} -v Nsolvent=${Nsolvent} -v Npoly=full \
+     -f addpolymer.awk poly3d.txt > poly3.txt
+ nbound=$(tail -n 1 poly3.txt | awk '{print $1}')
+ sed "s/_NUMBER_OF_BOUNDS_/$nbound/1" poly3.txt > poly3d.txt
+
+# output directory name
+
+mkdir -p ${dname}
+${mpirun} -np ${nproc} ${lmp} ${vars} -in sdpd-polymer-run.lmp
