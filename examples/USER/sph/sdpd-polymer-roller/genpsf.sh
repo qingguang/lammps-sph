67d88c21bc10d4b95fac75e16b0b16d359e81638
Litvinov Sergey <slitvinov@gmail.com>
2013-03-21 15:20:18 +0100

    Update of the deposit.tcl

--- examples/USER/sph/sdpd-polymer-roller/genpsf.sh
+++ examples/USER/sph/sdpd-polymer-roller/genpsf.sh
@@ -3,12 +3,13 @@
 set -e
 set -u
 
+awk -f ../script/addmollabel.awk poly3d.txt poly3d.txt poly3d.txt > poly3m.txt
 tmpfile=$(mktemp /tmp/XXXXX)
 output=poly3d.psf
 # generate psf file with vmd
 vmd -dispdev text -eofexit <<EOF
 package require topotools
-topo readlammpsdata  poly3d.txt full
+topo readlammpsdata  poly3m.txt full
 animate write psf ${tmpfile}
 EOF
 
