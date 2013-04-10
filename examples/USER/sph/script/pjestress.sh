1eb1c648194dbad5fefd210c89e0bc149f504f34
xieq <xieq@ubuntu.(none)>
2013-04-10 21:42:14 +0700

    before pull

--- examples/USER/sph/script/pjestress.sh
+++ examples/USER/sph/script/pjestress.sh
@@ -1,5 +1,5 @@
-rm projection.*
-rm spectra.dat
+#rm projection.*
+#rm spectra.dat
 for filename in stress*.dat; do echo $filename;
 #Format: zs_sph_projection num_dim num_particles L Overlap input_filename file_type kernel_type projection_order
 #awk 'fl{print $3, $4, $6, $7} /ITEM: ATOMS/{fl=1}' $filename > ${filename/.dat/.del};
