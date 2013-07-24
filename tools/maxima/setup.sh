8dde4033be0aa5402c282bb6ab5d46f9df047a73
Litvinov Sergey <slitvinov@gmail.com>
2013-07-24 21:55:57 +0200

    Add fix_meso_constrained (a copy of fix_meso). Add maxima tools.

--- tools/maxima/setup.sh
+++ tools/maxima/setup.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
+# add directoreis to maxima search path
 
+minitfile=${HOME}/.maxima/maxima-init.mac
 lmp_maxima_path="[\"$(pwd)\", 
                   \"$(pwd)/nint\"]"
 
 # append content to maxima-init.mac file
 
->> ~/.maxima/maxima-init.mac cat <<EOF
+touch ${minitfile}
+>> ${minitfile} cat <<EOF
 /* add maxima-lammps tools to the search file */
 addlmppath(p):= (
   file_search_maxima: append (file_search_maxima,
@@ -13,4 +16,5 @@ addlmppath(p):= (
   file_search_lisp: append (file_search_lisp,
     [concat(p, "/###.lisp")]))$
 map('addlmppath, ${lmp_maxima_path})$
+kill(addlmppath)$
 EOF
