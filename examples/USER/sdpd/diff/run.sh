f8023007b46dd6d9b415647fbb3fbd3a09877ffc
LItvinov Sergey <Litvinov Sergey>
2013-08-08 12:01:50 +0200

    For kana

--- examples/USER/sdpd/diff/run.sh
+++ examples/USER/sdpd/diff/run.sh
@@ -10,18 +10,19 @@ else
     exit -1
 fi
 
-nproc=8
+nproc=1
 ndim=2
 sdpd_eta=8.0
 sdpd_background=0.0
-sdpd_c=5e2
+sdpd_c=5e3
 nx=30
-n=4.0
-dname=c${sdpd_c}-ndim${ndim}-eta${sdpd_eta}-sdpd_background${sdpd_background}-nx${nx}-n${n}
+n=6.0
+ktype=laguerregaussian
+dname=c${sdpd_c}-ndim${ndim}-eta${sdpd_eta}-sdpd_background${sdpd_background}-nx${nx}-n${n}-ktype${ktype}
 
 vars="-var ndim ${ndim} -var dname ${dname} -var sdpd_c ${sdpd_c} \
       -var nx   ${nx} -var sdpd_eta ${sdpd_eta} -var sdpd_background ${sdpd_background} \
-      -var  n   ${n}"
+      -var  n   ${n} -var ktype ${ktype}"
 
 mkdir -p ${dname}
 ${mpirun} -np ${nproc} ${lmp} ${vars} -in solvent.lmp
