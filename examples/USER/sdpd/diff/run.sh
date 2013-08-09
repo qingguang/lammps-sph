717f7e2c56b728f5e7f3af7498c6bbe965c98467
Litvinov Sergey <Litvinov Sergey>
2013-08-09 16:53:10 +0200

    Add a template for laguerre-wendland kernel
    Former-commit-id: 2023aca38b07a6f12f9ac32d15c8fccf88a460eb

--- examples/USER/sdpd/diff/run.sh
+++ examples/USER/sdpd/diff/run.sh
@@ -10,14 +10,14 @@ else
     exit -1
 fi
 
-nproc=8
+nproc=1
 ndim=2
 sdpd_eta=8.0
 sdpd_background=0.0
 sdpd_c=1e3
 nx=30
 n=4.0
-ktype=wendland6
+ktype=quintic
 
 dname=c${sdpd_c}-ndim${ndim}-eta${sdpd_eta}-sdpd_background${sdpd_background}-nx${nx}-n${n}-ktype${ktype}
 
