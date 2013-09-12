ff6be9c140ffebc18b8656947fe55445b3dd5e62
LItvinov Sergey <Litvinov Sergey>
2013-09-12 18:20:28 +0200

    Add laguerrewendland4

--- src/USER-SDPD/sph_kernel_laguerrewendland4.cpp
+++ src/USER-SDPD/sph_kernel_laguerrewendland4.cpp
@@ -41,9 +41,9 @@ double LAMMPS_NS::SPHKernelLaguerreWendland4::dw3d(double r) {
   const double norm3d = 9009/(64.0*MathConst::MY_PI);
   if (r<1.0) {
     return  norm3d* (  
-		     (5670*pow(r,11)-28512*pow(r,10)+53550*pow(r,9)-38880*pow(r,8)-7280*pow(r,7)
-		      +26880*pow(r,6)-11088*pow(r,5)-2240*pow(r,4)+2010*pow(r,3)
-		      -110*r)
+		     -6*pow(1-r,5)*(35*pow(r,2)/3+6*r+1)*(27*pow(r,4)/2-9*pow(r,2)+1)
+		     +pow(1-r,6)*(70*r/3+6)*(27*pow(r,4)/2-9*pow(r,2)+1)
+		     +pow(1-r,6)*(35*pow(r,2)/3+6*r+1)*(54*pow(r,3)-18*r)
 		       );
   } else {
     return 0.0;
@@ -54,9 +54,9 @@ double LAMMPS_NS::SPHKernelLaguerreWendland4::dw2d(double r) {
   const double norm2d = 1287/(47.0*MathConst::MY_PI);
   if (r<1.0) {
     return  norm2d* (  
-		     (5670*pow(r,11)-28512*pow(r,10)+53550*pow(r,9)-38880*pow(r,8)-7280*pow(r,7)
-		      +26880*pow(r,6)-11088*pow(r,5)-2240*pow(r,4)+2010*pow(r,3)
-		      -110*r)
+		     -6*pow(1-r,5)*(35*pow(r,2)/3+6*r+1)*(27*pow(r,4)/2-9*pow(r,2)+1)
+		     +pow(1-r,6)*(70*r/3+6)*(27*pow(r,4)/2-9*pow(r,2)+1)
+		     +pow(1-r,6)*(35*pow(r,2)/3+6*r+1)*(54*pow(r,3)-18*r)
 		       );
   } else {
     return 0.0;
