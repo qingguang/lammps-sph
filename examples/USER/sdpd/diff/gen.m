08f70053af1da940186dcc9766e93261d08174cf
Litvinov Sergey <slitvinov@gmail.com>
2013-10-17 16:50:22 +0200

    Update of the test cases
    Former-commit-id: a1914646cfb1fe256a1d0aa8068dd00f889ea1b5

--- examples/USER/sdpd/diff/gen.m
+++ examples/USER/sdpd/diff/gen.m
@@ -1,4 +1,4 @@
-sdpd_c = logspace(-1, 5, 20);
+sdpd_c = logspace(-1, 5, 200);
 n = linspace(2.5, 5.0, 10);
 dlmwrite("sdpd_c.dat", sdpd_c', " ", "precision", "%6.2e")
 dlmwrite("n.dat", n', " ", "precision", "%6.2e")
