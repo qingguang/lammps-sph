a61704cd61d7ca39a29e9b376f0021b8b72b1bdd
LItvinov Sergey <Litvinov Sergey>
2013-11-21 11:50:58 +0100

    Home

--- examples/USER/sdpd/simpleswimmer/postprocessing/simplepost.sh
+++ examples/USER/sdpd/simpleswimmer/postprocessing/simplepost.sh
@@ -7,8 +7,8 @@ source simplepost-utils.sh
 for f in $(find ../supermuc-data/ -name moments_swimmercom.dat); do
     d=$(dirname ${f})/../
     makemsd ${d}
-    slope=$(linreg ${d} 6.0 | awk '{print $1}')
-    echo $(getp ${d} polymer_concentration) $(getp ${d} v_wave) ${slope} >> concentration-v_wave-msdslope.dat
+    #slope=$(linreg ${d} 6.0 | awk '{print $1}')
+    #echo $(getp ${d} polymer_concentration) $(getp ${d} v_wave) ${slope} >> concentration-v_wave-msdslope.dat
 done
 
 
