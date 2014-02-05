b705454c53af941e6fe211c96b64c5c26dbda36c
Litvinov Sergey <slitvinov@gmail.com>
2014-02-05 21:59:21 +0100

    Run for g=0.0

--- examples/USER/sdpd/diff/run.sh
+++ examples/USER/sdpd/diff/run.sh
@@ -12,17 +12,18 @@ fi
 
 nproc=8
 ndim=2
-sdpd_eta=1e-1
+sdpd_eta=1e-5
 sdpd_background=1.0
-sdpd_c=1.00e+01
+sdpd_c=1.00
+sdpd_gamma=0.00
 nx=32
-n=3.50
-ktype=laguerre2wendland4eps
-#ktype=quintic
+n=3.00
+#ktype=laguerre2wendland4eps
+ktype=quintic
 
-dname=c${sdpd_c}-ndim${ndim}-eta${sdpd_eta}-sdpd_background${sdpd_background}-nx${nx}-n${n}-ktype${ktype}
+dname=c${sdpd_c}-gamma${sdpd_gamma}-eta${sdpd_eta}-sdpd_background${sdpd_background}-nx${nx}-n${n}-ktype${ktype}
 
-vars="-var ndim ${ndim} -var dname ${dname} -var sdpd_c ${sdpd_c} \
+vars="-var sdpd_gamma ${sdpd_gamma} -var ndim ${ndim} -var dname ${dname} -var sdpd_c ${sdpd_c} \
       -var nx   ${nx} -var sdpd_eta ${sdpd_eta} -var sdpd_background ${sdpd_background} \
       -var  n   ${n} -var ktype ${ktype}"
 
