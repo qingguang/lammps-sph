dd29a350230fa79772ac8a2e6ccd9dba4f26c101
Litvinov Sergey <slitvinov@gmail.com>
2014-02-11 22:05:51 +0100

    Run with temperature

--- examples/USER/sdpd/diff/run.sh
+++ examples/USER/sdpd/diff/run.sh
@@ -12,19 +12,20 @@ fi
 
 nproc=8
 ndim=2
-sdpd_eta=1e-1
-sdpd_background=0.99
+sdpd_eta=1.0
+sdpd_background=0.00
 sdpd_c=10.00
 sdpd_gamma=1.00
-nx=32
-n=4.00
+nx=30
+n=3.80
 ktype=wendland4
+temp=0.001
 
-dname=c${sdpd_c}-gamma${sdpd_gamma}-eta${sdpd_eta}-sdpd_background${sdpd_background}-nx${nx}-n${n}-ktype${ktype}
+dname=c${sdpd_c}-temp${temp}-gamma${sdpd_gamma}-eta${sdpd_eta}-background${sdpd_background}-nx${nx}-n${n}-ktype${ktype}
 
 vars="-var sdpd_gamma ${sdpd_gamma} -var ndim ${ndim} -var dname ${dname} -var sdpd_c ${sdpd_c} \
       -var nx   ${nx} -var sdpd_eta ${sdpd_eta} -var sdpd_background ${sdpd_background} \
-      -var  n   ${n} -var ktype ${ktype}"
+      -var  n   ${n} -var ktype ${ktype} -var temp ${temp}"
 
 mkdir -p ${dname}
 ${mpirun} -np ${nproc} ${lmp} ${vars} -in solvent.lmp
