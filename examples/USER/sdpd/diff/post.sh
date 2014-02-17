e9120397c16f1b7e1104e340c12580680880783e
Litvinov Sergey <Litvinov Sergey>
2014-02-18 00:38:55 +0100

    Post-processing

--- examples/USER/sdpd/diff/post.sh
+++ examples/USER/sdpd/diff/post.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for dname in $(find . -type d -name 'c1.44e2-temp0.25-gamma1.00-eta8.0-background0.00-nx30-n4.5-ktypelaguerrewendland4eps-grid*'); do
+for dname in $(find . -type d -name 'c*'); do
     dlast=$(ls -1 ${dname}/dump*.dat  | tail -n1)
     n=$(echo ${dname} | awk -v FS="-" '{print $NF}' | tr -d n)
     err=$(awk 'NF>=12{s+=$12; n++} END {print s/n}' ${dlast})
--- examples/USER/sdpd/diff/run.sh
+++ examples/USER/sdpd/diff/run.sh
@@ -10,16 +10,16 @@ else
     exit -1
 fi
 
-nproc=8
+nproc=1
 ndim=2
 sdpd_eta=8.0
 sdpd_background=0.00
 sdpd_c=1.44e2
 sdpd_gamma=1.00
-nx=30
-n=4.5
-ktype=laguerrewendland4eps
-temp=0.25
+nx=16
+n=3.0
+ktype=quintic
+temp=0.01
 grid=1
 
 dname=c${sdpd_c}-temp${temp}-gamma${sdpd_gamma}-eta${sdpd_eta}-background${sdpd_background}-nx${nx}-n${n}-ktype${ktype}-grid${grid}
