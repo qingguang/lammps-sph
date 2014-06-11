96b1760e8aae060d558b6caf12e751aab974c5cb
Sergey Litvinov <Litvinov Sergey>
2014-06-11 19:26:18 +0200

    Change localtion of Pizza.py

--- examples/USER/sph/bubble_growth/post.sh
+++ examples/USER/sph/bubble_growth/post.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 cd data-ndim3
-PYTHONPATH=/scratch/work/Pizza.py/src/ python ../../scripts/dump2ensight.py dump*.dat
+PYTHONPATH=${HOME}/work/Pizza.py/src/ python ../../scripts/dump2ensight.py dump*.dat
